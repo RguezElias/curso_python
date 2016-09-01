08ceb3e1dc210b9c82c46f1f7bcf0562394abdb9
RguezElias <eliasrodriguez1989@gmail.com>
2016-09-01 11:12:42 -0500

    Add files via upload

--- encapsulacion.py
+++ encapsulacion.py
@@ -1,22 +1,24 @@
-# principio de encapsulacion
+
+
+#principio de encapsulacion 
 
 class Animal(object):
-
 	def tipo(self):
 		return self.tipo
-	
+
 	def es_veloz(self):
 		return self.es_veloz
 
 	def __init__(self):
-		print 'fui creado'
+		print "Fui creado"
 		self.tipo = 'Animalia'
 
 		#atributo privado
-		self.__es_veloz = True
+		self._es_veloz = True
 
 leon = Animal()
 leon.es_veloz = False
 
 print leon.es_veloz
+
 
