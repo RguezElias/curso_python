b4f1c26a5de3ecaaf4c72c64bffe165187911b85
RguezElias <eliasrodriguez1989@gmail.com>
2016-09-01 11:11:46 -0500

    Add files via upload

--- dc_acceder.py
+++ dc_acceder.py
@@ -1,17 +1,18 @@
+#acceder a valores del diccionario
 
-#acceder valores diccionario
 
-pizza = {'tipo':'vegetariana','costo': 150, 'caliente': True}
+pizza = {'tipo':'vegetariana', 'costo':150, 'caliente':True}
 
 print pizza['tipo']
 
 print pizza['costo']
 
-#acceder a todos los valores de un diccionario
+#acceder a todos los valores de un diccionario 
 
 print pizza.values()
 
-#iterar valores diccionario
+#iterar valores en el diccionario
 
 for valor in pizza:
-	print valor
+
+    print valor
